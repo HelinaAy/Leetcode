222d05a8cddc521c83aa1d2dd2913bf21a88bbcb
Hilina Ayalew <hilinpro5@gmail.com>
2026-06-30 18:02:35 +0300

    Update TwoSum.py

--- TwoSum.py
+++ TwoSum.py
@@ -1,16 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # Create a dictionary to store the complement and its index
-        num_to_index = {}
-        
-        # Iterate through the list
-        for i, num in enumerate(nums):
-            # Calculate the complement needed to reach the target
-            complement = target - num
-            
-            # Check if the complement exists in the dictionary
-            if complement in num_to_index:
-                return [num_to_index[complement], i]
-            
-            # Otherwise, store the current number with its index
-            num_to_index[num] = i
+        prevMap = {} 
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
